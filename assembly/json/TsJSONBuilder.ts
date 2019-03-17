665fc3e78b766fa8342a4df074059d99287c3efc
Brett Chaldecott <brett.chaldecott@gmail.com>
2019-03-17 20:38:45 +0200

    1) update of libraries

--- assembly/json/TsJSONBuilder.ts
+++ assembly/json/TsJSONBuilder.ts
@@ -1,21 +1,40 @@
 import "~lib/number"
 
-export class TsJSONBuilder {
+export class TsJsonType {
+    static readonly BASIC : string = "BASIC";
+    static readonly ARRAY: string = "ARRAY";
+    static readonly OBJECT: string = "OBJECT";
+};
 
+export class TsJSONBuilder {
+    
+    type: string;
     key: string;
     value: string;
     values: Array<TsJSONBuilder>;
 
-    constructor(key: string = "") {
+    constructor(key: string = "", type: string = TsJsonType.OBJECT) {
+        this.type = type;
         this.key = key;
         this.value = "";
         this.values = new Array<TsJSONBuilder>();
     }
     
-    add(key: string = "") : TsJSONBuilder {
-        var entry = new TsJSONBuilder(key)
+    add(key: string = "", type: string = TsJsonType.OBJECT) : TsJSONBuilder {
+        var entry = new TsJSONBuilder(key,type)
         this.values.push(entry);
         return entry;
+    }
+
+    addArray(key: string = "") : TsJSONBuilder {
+        var entry = new TsJSONBuilder(key,TsJsonType.ARRAY)
+        this.values.push(entry);
+        return entry;
+    }
+
+    setKey(key: string) : TsJSONBuilder {
+        this.key = key;
+        return this;
     }
 
     set(value: string = "") : void {
@@ -52,13 +71,27 @@ export class TsJSONBuilder {
             result = this.wrappString(this.value);
         } else if (this.key.length != 0 && this.value.length !=0) {
             result = this.wrappString(this.key) + ":" + this.wrappString(this.value)
-        } else if (this.key.length == 0 && this.values.length !=0) {
+        } else if (this.type == TsJsonType.ARRAY && this.key.length != 0 && this.values.length !=0) {
+            result = this.wrappString(this.key) + ":["
+            for(let count = 0; count < this.values.length; count++) {
+                result += sep + this.values[count].generateJson()
+                sep = ","
+            }
+            result += "]"
+        } else if (this.type == TsJsonType.ARRAY && this.key.length == 0 && this.values.length !=0) {
             result = "["
             for(let count = 0; count < this.values.length; count++) {
                 result += sep + this.values[count].generateJson()
                 sep = ","
             }
             result += "]"
+        } else if (this.key.length == 0 && this.values.length !=0) {
+            result = "{"
+            for(let count = 0; count < this.values.length; count++) {
+                result += sep + this.values[count].generateJson()
+                sep = ","
+            }
+            result += "}"
         } else if (this.key.length != 0 && this.values.length !=0) {
             result = this.wrappString(this.key) + ":{"
             for(let count = 0; count < this.values.length; count++) {
